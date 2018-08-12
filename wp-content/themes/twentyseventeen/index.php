5afa15cc82a45a5b56dd9ab022c59ff98c23647d
Lee <number8pie@gmail.com>
2018-08-12 16:07:05 +0100

    Deploy HQ test.

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -18,6 +18,7 @@
 get_header(); ?>
 
 <div class="wrap">
+	<!-- Deploy HQ Test Message -->
 	<?php if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
 			<h1 class="page-title"><?php single_post_title(); ?></h1>
