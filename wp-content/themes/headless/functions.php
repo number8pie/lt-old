1ef444498ed29869c152bbc31e5a3efd30ca8c71
Lee <number8pie@gmail.com>
2018-08-26 15:17:07 +0100

    Adding custpom posts and images

--- wp-content/themes/headless/functions.php
+++ wp-content/themes/headless/functions.php
@@ -8,3 +8,6 @@ include(get_template_directory() . '/admin/custom_taxonomies.php');
 
 // Shortcodes
 include(get_template_directory() . '/admin/shortcodes.php');
+
+add_theme_support('post-thumbnails');
+add_action( 'after_switch_theme', 'flush_rewrite_rules' );
